ceddc7967a6beb5431db0f0dd630ccfab5a2e54e
ndisidore <ndisidore@gmail.com>
2012-09-06 14:03:37 -0500

    Following tutorial; added GoogleMaps.java; changed project type to GoogleAPIs

---
+++ src/com/eecs541/googlemapsintro/GoogleMaps.java
@@ -0,0 +1,45 @@
+package com.eecs541.googlemapsintro;
+
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+ 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class GoogleMaps extends MapActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher);
+		ItemizedOverlay itemizedOverlay = new ItemizedOverlay(
+				drawable, this);
+
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+
+		itemizedOverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedOverlay);
+
+		MapController mapController = mapView.getController();
+		mapController.animateTo(point);
+		mapController.setZoom(6);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+}
