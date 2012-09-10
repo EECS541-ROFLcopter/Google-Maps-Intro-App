e4c9388b70de587f33a0ddaea5eb630c37d81b78
Nathan Disidore <ndisidore@gmail.com>
2012-09-09 20:18:01 -0500

    Removed Unnecessary Activity Class

--- src/com/eecs541/googlemapsintro/GoogleMaps.java
+++ src/com/eecs541/googlemapsintro/GoogleMaps.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
  
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -36,6 +37,12 @@ public class GoogleMaps extends MapActivity {
 		mapController.animateTo(point);
 		mapController.setZoom(6);
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 	@Override
 	protected boolean isRouteDisplayed() {
